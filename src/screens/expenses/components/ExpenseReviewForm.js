5aaed0cf9a51c91873b0e511269ea854b0d58b64
Eduardo Garza <omaarg@gmail.com>
2019-11-17 18:03:21 -0600

    Add placeholders to form

--- src/screens/expenses/components/ExpenseReviewForm.js
+++ src/screens/expenses/components/ExpenseReviewForm.js
@@ -8,6 +8,7 @@ import {
   Box,
   MaskedInput
 } from 'grommet';
+import { DateTime } from 'luxon';
 
 function ExpenseReviewForm({ expense }) {
   const {
@@ -23,28 +24,45 @@ function ExpenseReviewForm({ expense }) {
     <Box>
       <Form>
         <FormField label='Nota'>
-          <TextArea name='note' defaultValue={note} />
+          <TextArea
+            autoFocus
+            name='note'
+            defaultValue={note}
+            placeholder='Cena del viernes en la noche' />
         </FormField>
         <FormField label='Categoría'>
-          <Select name='category' options={['Comida']} />
+          <Select
+            name='category'
+            options={['Comida']}
+            placeholder='Alimentación' />
         </FormField>
         <FormField label='Fecha'>
-          <MaskedInput name='date' defaultValue={date.toLocaleString()} />
+          <MaskedInput
+            name='date'
+            defaultValue={date.toLocaleString()}
+            placeholder={DateTime.local().toLocaleString()}
+          />
         </FormField>
         <FormField label='Hora'>
-          <MaskedInput name='hour'></MaskedInput>
+          <MaskedInput
+            name='hour'
+            defaultValue={date.toLocaleString()}
+            placeholder={DateTime.local().toLocaleString(DateTime.TIME_24_SIMPLE)} />
         </FormField>
         <FormField label='Cantidad'>
-          <MaskedInput name='amount' defaultValue={amount} />
+          <MaskedInput
+            name='amount'
+            defaultValue={amount.toFixed(2)}
+            placeholder='$ 200.00' />
         </FormField>
       </Form>
       <Button
+        label='Terminar'
         primary
         type='submit'
-        label='Enviar'
         margin='medium'
         alignSelf='center' />
-    </Box>
+    </Box >
   );
 }
 
