c7956d70ce2af30ae3d167b0411ac61fa1c5b781
Eduardo Garza <omaarg@gmail.com>
2020-03-02 20:22:48 -0600

    Use correct function name

--- src/foundation/computeTotalAmount.test.js
+++ src/foundation/computeTotalAmount.test.js
@@ -41,5 +41,5 @@ it('should compute total amount for a given list of expenses', () => {
 
   const actual = computeTotalAmount(expenses);
 
-  expect(actual).toStrict(expected);
+  expect(actual).toEqual(expected);
 });
