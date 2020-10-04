96674f958036a7b6ab8a7089846619270fe4b347
Eduardo Garza <omaarg@gmail.com>
2020-10-04 11:29:52 -0500

    Make login screen use screen components

--- src/auth/components/LoginScreen.js
+++ src/auth/components/LoginScreen.js
@@ -2,17 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Shapes from '../shapes';
 import { Box, Heading, Text } from 'grommet';
+import { Screen, ScreenBody } from '../../foundation/components/screen';
 import LoginForm from './LoginForm';
 
 function LoginScreen(props) {
   return (
-    <Box fill='vertical' justify='center'>
-      <Text alignSelf='center'><span role='img' aria-label='gaston logo'>💵</span></Text>
-      <Heading level='2' alignSelf='center'>Gaston</Heading>
-      <Box pad='large'>
-        <LoginForm logIn={props.logIn} loginStatus={props.loginStatus} />
-      </Box>
-    </Box >
+    <Screen>
+      <ScreenBody>
+        <Box fill='vertical' justify='center'>
+          <Text alignSelf='center'><span role='img' aria-label='gaston logo'>💵</span></Text>
+          <Heading level='2' alignSelf='center'>Gaston</Heading>
+          <Box pad='large'>
+            <LoginForm logIn={props.logIn} loginStatus={props.loginStatus} />
+          </Box>
+        </Box>
+      </ScreenBody>
+    </Screen>
   );
 }
 
